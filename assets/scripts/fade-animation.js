5230589b54c220856f72e9bfe7ac7a93f606fbfc
Sasha <sashavikhorevaa@gmail.com>
2023-08-26 00:26:40 +0300

    feat : add main page

---
+++ assets/scripts/fade-animation.js
@@ -0,0 +1,5 @@
+
+$(function () {
+    $('.main-content').fadeIn(1500);
+    $('.header__item').fadeIn(1000);
+})
---
+++ assets/scripts/gallery-slider.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        speed: 300,
+        easing: 'ease',
+        cssEase: 'ease-in',
+        responsive: [
+            {
+                breakpoint: 769,
+                settings: {
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 481,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+});
+
+
