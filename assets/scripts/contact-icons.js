bf74879b35aef0cc844d0f562d8eacb26f1a7e55
Sasha <sashavikhorevaa@gmail.com>
2023-09-05 15:01:06 +0300

    fix : fix blocks

--- assets/scripts/contact-icons.js
+++ assets/scripts/contact-icons.js
@@ -1,5 +1,4 @@
-let whats = document.querySelector(".whats");
-let mail = document.querySelector(".mail");
+
 let up = document.querySelector(".up");
 
 window.onscroll = function () {
@@ -11,27 +10,12 @@ function scrollFunction() {
     document.body.scrollTop > 100 ||
     document.documentElement.scrollTop > 100
   ) {
-    whats.style.display = "block";
-    mail.style.display = "block";
     up.style.display = "block";
   } else {
-    whats.style.display = "none";
-    mail.style.display = "none";
     up.style.display = "none";
-  }
-  if(window.innerWidth <= 480){
-    mail.style.display = "none";
   }
 }
 
-
-
-$('body').on('click', '.whats', function(e){
-  window.open('http://wa.me/79037293137','_blank');
-})
-$('body').on('click', '.mail', function(e){
-  document.location.href = 'mailto:support@digsol.tech?subject=Заявка с сайта';
-})
 $('body').on('click', '.up', function(e){
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
